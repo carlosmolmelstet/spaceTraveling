80ab0e65bee5e19484665791afe2ca2e3bd0b14c
Carlos Molmelstet <molmelstet2016@gmail.com>
2021-10-04 22:14:17 -0300

    configurando o prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic  from '@prismicio/client';
+import { RichText }  from 'prismic-dom';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -20,17 +22,47 @@ interface PostPagination {
   results: Post[];
 }
 
-interface HomeProps {
-  postsPagination: PostPagination;
+interface Posts2 {
+  slug: string;
+  title:  string;
+  subtitle:string;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+interface HomeProps {
+  postsPagination: Posts2[];
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export default function Home({postsPagination}: HomeProps) {
+  console.log(postsPagination);
+  return(
+    <>
+     {postsPagination.map(post => (
+       <div key={post.slug}>
+        <h1>{post.title}</h1>
+        <p>{post.subtitle}</p>
+       </div>
+      ))}
+    
+    </>
+  )
+}
 
-//   // TODO
-// };
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([Prismic.Predicates.at('document.type', 'posts')]);
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+      title: post.data.title,
+      subtitle:post.data.subtitle,
+    }
+  })
+
+
+  return {
+    props: {
+      postsPagination: posts
+    }
+  } 
+};
